b0068fb615187d6e94da88e48694f7c81fb13073
Sanchit Narang <sanchit.narang@axeno.com>
2026-08-18 23:49:33 +0530

    gap

--- blocks/content-cards/content-cards.js
+++ blocks/content-cards/content-cards.js
@@ -99,7 +99,7 @@ export default function decorate(block) {
       new Splide(splideEl, {
         type: 'loop',
         perPage: 3,
-        gap: '1rem',
+        gap: '1.5rem',
         pagination: true,
         arrows: false,
         breakpoints: {
